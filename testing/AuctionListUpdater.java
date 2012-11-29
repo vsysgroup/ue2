18b84de9dea51bee418225d27415c0f187fe511c
Babz <Babz@Babz-Laptop.(none)>
2012-11-29 15:20:14 +0100

    list update

--- testing/AuctionListUpdater.java
+++ testing/AuctionListUpdater.java
@@ -1,9 +1,13 @@
 package testing;
 
+import org.apache.log4j.Logger;
+
 import server.Server;
 
 public class AuctionListUpdater implements Runnable {
 
+	public static final Logger LOG = Logger.getLogger(AuctionListUpdater.class);
+	
 	private int updateInterval;
 	private AuctionBidder auctionBidder;
 
@@ -17,6 +21,7 @@ public class AuctionListUpdater implements Runnable {
 
 		while (true) {
 			auctionBidder.randomAuction = Server.getRandomAuction();
+			LOG.info("list update");
 			try {
 				Thread.sleep(updateInterval*1000);
 			} catch (InterruptedException e) {
