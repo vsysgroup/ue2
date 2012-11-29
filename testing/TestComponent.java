617a90a2bbb4ae0c16c8c857370531cf5ae11a2a
Babz <Babz@Babz-Laptop.(none)>
2012-11-29 11:15:18 +0100

    first steps for testing

--- testing/TestComponent.java
+++ testing/TestComponent.java
@@ -7,9 +7,23 @@ package testing;
  */
 public class TestComponent implements Runnable {
 
+	private int auctionsPerMin;
+	private int auctionDuration;
+	private int updateInterval;
+	private int bidsPerMin;
+
+	public TestComponent(int auctionsPerMin, int auctionDuration,
+			int updateIntervalSec, int bidsPerMin) {
+		this.auctionsPerMin = auctionsPerMin;
+		this.auctionDuration = auctionDuration;
+		updateInterval = updateIntervalSec;
+		this.bidsPerMin = bidsPerMin;
+	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+
+		//auction creation
 		
 	}
 
--- testing/Tester.java
+++ testing/Tester.java
@@ -4,27 +4,39 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
-
+/**
+ * creates a new thread for each testing client
+ * @author Barbara Schwankl 0852176
+ *
+ */
 public class Tester {
 
 	private final static ExecutorService threadpool = Executors.newCachedThreadPool();
 
 	public static void main(String[] args) {
 
-		//Management
-		//eventSubscription on any event type (filter ".*")
-		//auto mode
-
 		LoadtestReader testParameters = new LoadtestReader();
+		
+		//number of concurrent bidding client
 		int noOfClients = testParameters.getNoOfClients();
+		//number of started auctions per client per minute
 		int auctionsPerMin = testParameters.getAuctionsPerMin();
+		//duration of the auctions in seconds 
 		int auctionDuration = testParameters.getAuctionDuration();
+		//number of seconds that have to pass before the clients repeatedly update the current list of active auctions
 		int updateIntervalSec = testParameters.getUpdateIntervalSec();
+		//number of bids placed on random auctions per client per minute
 		int bidsPerMin = testParameters.getBidsPerMin();
 
+		//one mgmt client
+		//eventSubscription on any event type (filter ".*")
+		//auto mode
+//		threadpool.execute(new TestComponent());
+		
+		//several clients
 		for(int i = 0; i <= noOfClients; ++i) {
 
-			threadpool.execute(new TestComponent());
+			threadpool.execute(new TestComponent(auctionsPerMin, auctionDuration, updateIntervalSec, bidsPerMin));
 		}
 	}
 
